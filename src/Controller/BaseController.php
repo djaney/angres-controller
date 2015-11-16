a47916b1a32c467330c3feddfe43a13d1e09708b
Djane Rey Mabelin <d.mabelin@arcanys.com>
2015-11-16 14:08:03 +0800

    refactor controller to not use DI for route parameters

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -1,7 +1,6 @@
 <?php namespace Angres\Bundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
-use Symfony\Component\HttpFoundation\Request;
 
 abstract class BaseController extends FOSRestController {
 
@@ -13,27 +12,34 @@ abstract class BaseController extends FOSRestController {
         return $this->handleView($view);
     }
 
-    public function getAction($id){
+    public function getAction(){
+        $id = $this->container->get('request')->attributes->get('id');
         $data = $this->get($this->facade)->get($id);
         $view = $this->view($data);
         return $this->handleView($view);
     }
 
-    public function postAction( Request $req){
-        $data = $this->get($this->facade)->post($this->container->get('request')->request->all());
+    public function postAction(){
+        $body = $this->container->get('request')->request->all();
+
+        $data = $this->get($this->facade)->post($body);
         $view = $this->view($data);
         return $this->handleView($view);
     }
 
-    public function patchAction($id){
-        $data = $this->get($this->facade)->patch($id,$this->container->get('request')->request->all());
+    public function patchAction(){
+        $id = $this->container->get('request')->attributes->get('id');
+        $body = $this->container->get('request')->request->all();
+
+        $data = $this->get($this->facade)->patch($id,$body);
         $view = $this->view($data);
         return $this->handleView($view);
 
     }
 
+    public function deleteAction(){
+        $id = $this->container->get('request')->attributes->get('id');
 
-    public function deleteAction($id){
         $data = $this->get($this->facade)->delete($id);
         $view = $this->view($data);
         return $this->handleView($view);
